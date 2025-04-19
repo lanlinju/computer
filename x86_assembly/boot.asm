03b596101d907a580aba36606bbb33a1599f9d7a
Lanlinju <lanlinju@outlook.com>
2025-04-19 17:30:38 +0800

    Enable Bochs debugger and IO debug
    - Update bochsrc to enable iodebug and magic_break
    - Modify boot.asm to initialize registers for debugging
    - Update makefile to start Bochs with debugger

--- x86_assembly/boot.asm
+++ x86_assembly/boot.asm
@@ -1,21 +1,32 @@
 mov ax, 3
 int 0x10    ; 设置显示模式为80x25文本模式
 
-mov ax, 0xb800
-mov ds, ax; 将代码段设置为 0xb800
+xchg bx, bx
 
-mov byte [0], 'H'
-mov byte [2], 'e'
-mov byte [4], 'l'
-mov byte [6], 'l'
-mov byte [8], 'o'
-mov byte [10], ' '
-mov byte [12], 'W'
-mov byte [14], 'o'
-mov byte [16], 'r'
-mov byte [18], 'l'
-mov byte [20], 'd'
-mov byte [22], '!'
+mov ax, 0x1111
+mov bx, 0x2222
+mov cx, 0x3333
+mov dx, 0x4444
+mov di, 0x5555
+mov si, 0x6666
+mov sp, 0x7777
+mov bp, 0x8888
+
+; mov ax, 0xb800
+; mov ds, ax; 将代码段设置为 0xb800
+
+; mov byte [0], 'T'
+; mov byte [2], 'e'
+; mov byte [4], 'l'
+; mov byte [6], 'l'
+; mov byte [8], 'o'
+; mov byte [10], ' '
+; mov byte [12], 'W'
+; mov byte [14], 'o'
+; mov byte [16], 'r'
+; mov byte [18], 'l'
+; mov byte [20], 'd'
+; mov byte [22], '!'
 
 ; 下面阻塞停下来
 halt:
@@ -23,7 +34,3 @@ halt:
 
 times 510 - ($ - $$) db 0 ; 用 0 填充满 510 个字节
 db 0x55, 0xaa; 主引导扇区最后两个字节必须是 0x55aa
-
-; nasm -f bin -o hello.bin hello.asm
-; dd if=hello.bin of=master.img bs=512 count=1 conv=notrunc
-; bochs -q
