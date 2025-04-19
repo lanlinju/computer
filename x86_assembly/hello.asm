5529be3449c71e869527af44c1e4d51f6534c4b3
Lanlinju <lanlinju@outlook.com>
2025-04-19 14:59:50 +0800

    docs(x86_assembly): update hello.asm to display 'Hello World'

--- x86_assembly/hello.asm
+++ x86_assembly/hello.asm
@@ -1,7 +1,21 @@
+mov ax, 3
+int 0x10    ; 设置显示模式为80x25文本模式
+
 mov ax, 0xb800
 mov ds, ax; 将代码段设置为 0xb800
 
-mov byte [0], 'T'; 修改屏幕第一个字符为 T
+mov byte [0], 'H'
+mov byte [2], 'e'
+mov byte [4], 'l'
+mov byte [6], 'l'
+mov byte [8], 'o'
+mov byte [10], ' '
+mov byte [12], 'W'
+mov byte [14], 'o'
+mov byte [16], 'r'
+mov byte [18], 'l'
+mov byte [20], 'd'
+mov byte [22], '!'
 
 ; 下面阻塞停下来
 halt:
@@ -9,3 +23,7 @@ halt:
 
 times 510 - ($ - $$) db 0 ; 用 0 填充满 510 个字节
 db 0x55, 0xaa; 主引导扇区最后两个字节必须是 0x55aa
+
+; nasm -f bin -o hello.bin hello.asm
+; dd if=hello.bin of=master.img bs=512 count=1 conv=notrunc
+; bochs -q
