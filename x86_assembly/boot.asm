5d3230379a99d1f33a27b18bce07260b9a77ac33
Lanlinju <lanlinju@outlook.com>
2025-05-05 16:16:08 +0800

    Refactor boot.asm: remove unused code and clean up comments

--- x86_assembly/boot.asm
+++ x86_assembly/boot.asm
@@ -5,32 +5,22 @@ int 0x10    ; 设置显示模式为80x25文本模式
 
 xchg bx, bx
 
-mov ax, 0xb800
-mov es, ax
 
-mov ax, 0
-mov ds, ax
+; mov ax, 5
+; mov bx, 6
+; add ax, bx; ax = ax + bx
 
-mov si, message
-mov di, 0
-mov cx, message_end - message
+; add word [number], 0x1000 
 
-loop1:
-    mov al, [ds:si]
-    mov [es:di], al
-
-    inc si
-    add di, 2
-
-    loop loop1
+mov ax, 5
+mov bx, 7
+mul bx; dx:ax = ax * bx
 
 halt:
     jmp halt
 
-message:
-    db "hello world!!!", 0
-message_end:
-    
+number:
+    dw 0x3456
 
 times 510 - ($ - $$) db 0 
 db 0x55, 0xaa; 主引导扇区最后两个字节必须是 0x55aa
